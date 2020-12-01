c908f025c66b81fb99afa7b73f93925f8ec7e930
MistressAlison <alisonmoons@gmail.com>
2020-12-01 19:17:04 -0400

    Fixed comparing to BaseMod.MAX_HAND_SIZE

--- OrangeJuiceTheSpire/src/main/java/Moonworks/actions/MeltingMemoriesAction.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/actions/MeltingMemoriesAction.java
@@ -60,7 +60,7 @@ public class MeltingMemoriesAction extends AbstractGameAction {
                     disCard.current_x = -1000.0F * Settings.scale;
                     //disCard2.current_x = -1000.0F * Settings.scale + AbstractCard.IMG_HEIGHT_S;
                     if (this.amount == 1) {
-                        if (AbstractDungeon.player.hand.size() < 10) {
+                        if (AbstractDungeon.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
                             AbstractDungeon.effectList.add(new ShowCardAndAddToHandEffect(disCard, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
                         } else {
                             AbstractDungeon.effectList.add(new ShowCardAndAddToDiscardEffect(disCard, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));

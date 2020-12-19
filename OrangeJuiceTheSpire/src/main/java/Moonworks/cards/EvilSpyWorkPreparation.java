c1f20941fae5766eb9b4ec604827bfef7478f05e
MistressAlison <alisonmoons@gmail.com>
2020-12-19 19:51:28 -0400

    EvilSpyWorkPreparation dropped to Common to help with the lack of multi target and common cards

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/EvilSpyWorkPreparation.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/EvilSpyWorkPreparation.java
@@ -22,7 +22,7 @@ public class EvilSpyWorkPreparation extends AbstractDynamicCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;

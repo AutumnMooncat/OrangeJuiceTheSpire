6e05210db2bb5e02159da3c27db69ccc1d4d494a
MistressAlison <alisonmoons@gmail.com>
2020-12-19 19:58:12 -0400

    Flamethrower dropped to Common. This is probably a bad idea

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/Flamethrower.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/Flamethrower.java
@@ -42,7 +42,7 @@ public class Flamethrower extends AbstractGiftCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;

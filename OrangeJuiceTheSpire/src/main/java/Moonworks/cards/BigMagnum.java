6fc0cf5d6a4785f8cd2d02dd73d641fa065b0a49
MistressAlison <alisonmoons@gmail.com>
2020-12-20 15:43:55 -0400

    Big Magnum rework. Now deals 5(6) damage for 7(10) Vigor and 0(1)+NormaLevel Strength

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/BigMagnum.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/BigMagnum.java
@@ -6,7 +6,9 @@ import Moonworks.characters.TheStarBreaker;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
@@ -19,6 +21,11 @@ public class BigMagnum extends AbstractNormaAttentiveCard {
 
     public static final String ID = OrangeJuiceMod.makeID(BigMagnum.class.getSimpleName());
     public static final String IMG = makeCardPath("BigMagnum.png");
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String UPGRADE1NAME = cardStrings.EXTENDED_DESCRIPTION[0];
+    public static final String UPGRADE2NAME = cardStrings.EXTENDED_DESCRIPTION[1];
 
     // /TEXT DECLARATION/
 
@@ -33,16 +40,20 @@ public class BigMagnum extends AbstractNormaAttentiveCard {
     private static final int COST = 0;
 
     private static final int HPLOSS = 5;
+    private static final int UPGRADE_PLUS_HPLOSS = 1;
 
-    private static final int STR = 3;
-    private static final int UPGRADE_PLUS_STR = 2;
+    private static final int VIGOR = 7;
+    private static final int UPGRADE_PLUS_VIGOR = 3;
+
+    private static final int STR = 0;
+    private static final int UPGRADE_PLUS_STR = 1;
 
     // /STAT DECLARATION/
 
     public BigMagnum() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-
-        magicNumber = baseMagicNumber = HPLOSS;
+        invertedNumber = baseInvertedNumber = HPLOSS;
+        magicNumber = baseMagicNumber = VIGOR;
         defaultSecondMagicNumber = defaultBaseSecondMagicNumber = STR;
         this.exhaust = true;
     }
@@ -50,14 +61,33 @@ public class BigMagnum extends AbstractNormaAttentiveCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(p, p, magicNumber));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new StrengthPower(p, defaultSecondMagicNumber),defaultSecondMagicNumber));
-        int vigor = getNormaLevel() >= 2 ? 5 : getNormaLevel() >= 1 ? 2 : 0;
-        if (vigor > 0) {
-            this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, vigor)));
-        }
-        //AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new LoseStrengthPower(p, defaultSecondMagicNumber),defaultSecondMagicNumber));
+        this.addToBot(new LoseHPAction(p, p, invertedNumber));
+        this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, magicNumber), magicNumber));
+        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, defaultSecondMagicNumber), defaultSecondMagicNumber));
+    }
 
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        this.defaultSecondMagicNumber = this.defaultBaseSecondMagicNumber;
+        this.isDefaultSecondMagicNumberModified = false;
+        initializeDescription();
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster m) {
+        super.calculateCardDamage(m);
+        this.defaultSecondMagicNumber = this.defaultBaseSecondMagicNumber + getNormaLevel();
+        this.isDefaultSecondMagicNumberModified = getNormaLevel() != 0;
+        initializeDescription();
+    }
+
+    @Override
+    protected void upgradeName() {
+        ++this.timesUpgraded;
+        this.upgraded = true;
+        this.name = UPGRADE1NAME;
+        this.initializeTitle();
     }
 
     // Upgraded stats.
@@ -65,6 +95,8 @@ public class BigMagnum extends AbstractNormaAttentiveCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
+            this.upgradeMagicNumber(UPGRADE_PLUS_VIGOR);
+            this.upgradeInvertedNumber(UPGRADE_PLUS_HPLOSS);
             this.upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_STR);
             this.initializeDescription();
         }

d25fc68cb512a1e78ac2069933a4551a61b924c8
MistressAlison <alisonmoons@gmail.com>
2021-01-09 12:09:43 -0400

    Added image container, images are used in Cloud of Seagulls and Jonathan Rush

---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/vfx/VFXContainer.java
@@ -0,0 +1,14 @@
+package Moonworks.vfx;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class VFXContainer {
+    public static Texture GULL_ATTACK_TEXTURE = new Texture("MoonworksResources/images/vfx/GullAttack.png");
+    public static Texture GULL_HURT_TEXTURE = new Texture("MoonworksResources/images/vfx/GullHurt.png");
+    public static Texture GULL_IDLE_TEXTURE = new Texture("MoonworksResources/images/vfx/GullIdle.png");
+    public static Texture GULL_KO_TEXTURE = new Texture("MoonworksResources/images/vfx/GullKO.png");
+    public static Texture GULL_ATTACK_TEXTURE_FLIP = new Texture("MoonworksResources/images/vfx/GullAttackFlipX.png");
+    public static Texture GULL_HURT_TEXTURE_FLIP = new Texture("MoonworksResources/images/vfx/GullHurtFlipX.png");
+    public static Texture GULL_IDLE_TEXTURE_FLIP = new Texture("MoonworksResources/images/vfx/GullIdleFlipX.png");
+    public static Texture GULL_KO_TEXTURE_FLIP = new Texture("MoonworksResources/images/vfx/GullKOFlipX.png");
+}

414a0b1be86a1e315dd4cc68010b44888facac68
MistressAlison <alisonmoons@gmail.com>
2021-01-11 04:13:32 -0400

    Saki's Cookie rework. In addition to new generator card, Sweet Battle

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/SakisCookie.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/SakisCookie.java
@@ -2,16 +2,24 @@ package Moonworks.cards;
 
 import Moonworks.cardModifiers.NormaDynvarModifier;
 import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
+import Moonworks.cards.giftCards.SweetBattle;
+import Moonworks.powers.SteadyPower;
 import Moonworks.powers.TemporaryDexterityPower;
+import Moonworks.powers.TemporaryStrengthPower;
 import basemod.helpers.CardModifierManager;
+import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import Moonworks.OrangeJuiceMod;
 import Moonworks.characters.TheStarBreaker;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static Moonworks.OrangeJuiceMod.makeCardPath;
 
@@ -21,6 +29,8 @@ public class SakisCookie extends AbstractNormaAttentiveCard {
 
     public static final String ID = OrangeJuiceMod.makeID(SakisCookie.class.getSimpleName());
     public static final String IMG = makeCardPath("SakisCookie.png");
+
+    private static ArrayList<TooltipInfo> OptimizationTooltip;
 
     // /TEXT DECLARATION/
 
@@ -35,7 +45,13 @@ public class SakisCookie extends AbstractNormaAttentiveCard {
     private static final int COST = 0;
 
     private static final int HEAL = 2;
-    private static final int UPGRADE_PLUS_HEAL = 2;
+    private static final int UPGRADE_PLUS_HEAL = 1;
+
+    private static final int STRDEX = 1;
+    private static final int UPGRADE_PLUS_STRDEX = 1;
+
+    private static final int VIGORSTEADY = 3;
+    private static final int UPGRADE_PLUS_VIGORSTEADY = 2;
 
     private static final Integer[] NORMA_LEVELS = {2};
 
@@ -44,27 +60,83 @@ public class SakisCookie extends AbstractNormaAttentiveCard {
     public SakisCookie() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, NORMA_LEVELS);
         magicNumber = baseMagicNumber = HEAL;
+        thirdMagicNumber = baseThirdMagicNumber = VIGORSTEADY;
         exhaust = true;
         this.tags.add(CardTags.HEALING);
-        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.MAGICMOD, 2, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
+        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INFOMOD, 0, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltipsTop() {
+        if (OptimizationTooltip == null)
+        {
+            OptimizationTooltip = new ArrayList<>();
+            OptimizationTooltip.add(new TooltipInfo(EXTENDED_DESCRIPTION[1], EXTENDED_DESCRIPTION[2]));
+        }
+        List<TooltipInfo> compoundList = new ArrayList<>(OptimizationTooltip);
+        if (super.getCustomTooltipsTop() != null) compoundList.addAll(super.getCustomTooltipsTop());
+        return compoundList;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int effect = AbstractDungeon.cardRandomRng.random(1, 3); //Choose a random number for the effect
+        int effect = effectLogic();
+        boolean sweetBattle = hasSweetBattle();
+        int boostEffect = sweetBattle ? 2 : 1;
         switch (effect) { //Switches allow us to run code based on the value in the switch.
             case 1: //Big Block. We multiply by 2 or 3 here
-                this.block *= magicNumber;
+                AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, magicNumber*boostEffect));
                 break; //Break after each case since we dont want it to then look at the other cases
             case 2: //Draw 2 or 3 cards, as this is stored in magicNumber
-                this.addToBot(new DrawCardAction(magicNumber));
+                this.addToBot(new ApplyPowerAction(p, p, new TemporaryDexterityPower(p, secondMagicNumber*boostEffect)));
+                this.addToBot(new ApplyPowerAction(p, p, new SteadyPower(p, thirdMagicNumber*boostEffect)));
                 break;
             case 3: //Buff 2 or 3 stacks of temp dex
-                this.addToBot(new ApplyPowerAction(p, p, new TemporaryDexterityPower(p, magicNumber)));
+                this.addToBot(new ApplyPowerAction(p, p, new TemporaryStrengthPower(p, secondMagicNumber*boostEffect)));
+                this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, thirdMagicNumber*boostEffect)));
                 break;
         }
-        AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, this.magicNumber));
+        if (sweetBattle) {
+            this.purgeOnUse = true;
+        }
+    }
+    private boolean hasSweetBattle() {
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            if (c instanceof SweetBattle) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int effectLogic() {
+        if (!upgraded) {
+            return AbstractDungeon.cardRandomRng.random(1, 3); //Choose a random number for the effect
+        } else {
+            if (AbstractDungeon.player.currentHealth < AbstractDungeon.player.maxHealth) {
+                return 1;
+            } else if (enemyAttacking() && hasBlockingCard()){
+                return 2;
+            } else {
+                return 3;
+            }
+        }
+    }
+    private boolean enemyAttacking() {
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if (m.getIntentBaseDmg() >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean hasBlockingCard() {
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            if (c.baseBlock > 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // Upgraded stats.
@@ -73,6 +145,8 @@ public class SakisCookie extends AbstractNormaAttentiveCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeMagicNumber(UPGRADE_PLUS_HEAL);
+            this.upgradeThirdMagicNumber(UPGRADE_PLUS_VIGORSTEADY);
+            //this.upgradeMagicNumber(UPGRADE_PLUS_HEAL);
             this.initializeDescription();
         }
     }
---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/giftCards/SweetBattle.java
@@ -0,0 +1,88 @@
+package Moonworks.cards.giftCards;
+
+import Moonworks.OrangeJuiceMod;
+import Moonworks.cards.SakisCookie;
+import Moonworks.cards.abstractCards.AbstractGiftCard;
+import Moonworks.characters.TheStarBreaker;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static Moonworks.OrangeJuiceMod.makeCardPath;
+
+public class SweetBattle extends AbstractGiftCard {
+
+    public static final Logger logger = LogManager.getLogger(OrangeJuiceMod.class.getName());
+
+    // TEXT DECLARATION
+
+    public static final String ID = OrangeJuiceMod.makeID(SweetBattle.class.getSimpleName());
+    public static final String IMG = makeCardPath("SweetBattle.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final AbstractCard.CardRarity RARITY = AbstractCard.CardRarity.UNCOMMON;
+    private static final AbstractCard.CardTarget TARGET = AbstractCard.CardTarget.NONE;
+    private static final AbstractCard.CardType TYPE = AbstractCard.CardType.SKILL;
+    public static final AbstractCard.CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
+
+    private static final int COST = -2;
+    private static final int CARDS = 1;
+
+    private static final int USES = 3;
+    private static final int UPGRADE_PLUS_USES = 1;
+
+    // /STAT DECLARATION/
+
+
+    public SweetBattle() {
+
+        this(USES, false);
+    }
+
+    public SweetBattle(int currentUses, boolean checkedGolden) {
+
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, USES, currentUses, checkedGolden);
+        this.magicNumber = this.baseMagicNumber = CARDS;
+        this.cardsToPreview = new SakisCookie();
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        super.triggerWhenDrawn();
+        if(active) {
+            this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview, 1, true, true));
+            this.applyEffect();
+        }
+    }
+
+    @Override
+    public void atTurnStartPreDraw() {
+        super.atTurnStartPreDraw();
+        if (active) {
+            this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview, 1, true, true));
+            this.applyEffect();
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            this.cardsToPreview.upgrade();
+            upgradeSecondMagicNumber(UPGRADE_PLUS_USES);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new SweetBattle(secondMagicNumber, checkedGolden);
+    }
+}

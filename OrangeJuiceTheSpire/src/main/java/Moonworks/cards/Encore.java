960145ae03db48615f0573cbf14c2417cd1c7d4f
MistressAlison <alisonmoons@gmail.com>
2021-01-11 19:42:45 -0400

    Added a new Power card, Encore. Encore is is 2(1) cost rare that saves a copy of every card you play during your turn, then activates the effect of them at the start of your next turn in order of most to least recently played. The amount of stacks of Encore dictates how many cards it will remember, and it does not wear off between turns.

---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/Encore.java
@@ -0,0 +1,73 @@
+package Moonworks.cards;
+
+import Moonworks.OrangeJuiceMod;
+import Moonworks.actions.AnotherUltimateWeaponAction;
+import Moonworks.cardModifiers.NormaDynvarModifier;
+import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
+import Moonworks.characters.TheStarBreaker;
+import Moonworks.powers.EncorePower;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Moonworks.OrangeJuiceMod.makeCardPath;
+
+public class Encore extends AbstractNormaAttentiveCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * In-Progress Form At the start of your turn, play a TOUCH.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = OrangeJuiceMod.makeID(Encore.class.getSimpleName());
+    public static final String IMG = makeCardPath("Encore.png");
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
+
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
+
+    private static final int STACKS = 1;
+
+    //private static final Integer[] NORMA_LEVELS = {-1};
+
+    // /STAT DECLARATION/
+
+
+    public Encore() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.magicNumber = this.baseMagicNumber = STACKS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new EncorePower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}
---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/powers/EncorePower.java
@@ -0,0 +1,141 @@
+package Moonworks.powers;
+
+import Moonworks.OrangeJuiceMod;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EncorePower extends AbstractPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = OrangeJuiceMod.makeID("EncorePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+
+    //Two arrayLists, because iterating backwards over a Map is awful
+    private final ArrayList<AbstractCard> cardArray = new ArrayList<>();
+    private final ArrayList<AbstractMonster> targetArray = new ArrayList<>();
+    //private boolean multiCardSupport = false;
+
+    //private AbstractCard lastCard;
+    //private AbstractMonster lastMonster;
+    private String lastName = "?";
+
+
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public EncorePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        // We load those txtures here.
+        this.loadRegion("hymn");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void onEnergyRecharge() {
+        super.onEnergyRecharge();
+
+        for (int i = 0 ; i < Math.min(amount, cardArray.size()) ; i++) {
+            //Get the original monster
+            AbstractMonster t = targetArray.get(i);
+
+            //If its null, dead, or otherwise gone, grab a new target.
+            //If the card has a null target to begin with, that it ought not interact with what we pass into the use function
+            //Still grab a non-null target anyway for safety
+            if (t == null || t.isDeadOrEscaped() || !AbstractDungeon.getMonsters().monsters.contains(t)) {
+                t = AbstractDungeon.getRandomMonster();
+            }
+
+            //Calculate the stuff
+            cardArray.get(i).applyPowers();
+            cardArray.get(i).calculateCardDamage(t);
+
+            //Use the card on the (new) target
+            cardArray.get(i).use(AbstractDungeon.player, t);
+        }
+
+        //Clear the arrays and wait for next time
+        cardArray.clear();
+        targetArray.clear();
+
+        //Clear the card text for next time
+        lastName = "?";
+
+        updateDescription();
+    }
+
+    //Fix scaling here?
+    public void stackPower(int stackAmount) {
+        super.stackPower(stackAmount);
+        updateDescription();
+    }
+
+    @Override
+    public void onPlayCard(AbstractCard card, AbstractMonster m) {
+        super.onPlayCard(card, m);
+
+        //Put a copy of every card played into the arrays
+        if(!card.purgeOnUse) {
+            cardArray.add(0, card);
+            targetArray.add(0, m);
+            lastName = getNameStrings();
+        }
+        //Update
+        updateDescription();
+    }
+
+    private String getNameStrings() {
+        if (cardArray.size() == 0) {
+            return "?";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ;  i < Math.min(amount, cardArray.size()) ; i++) {
+            sb.append(cardArray.get(i).name).append(i < (amount - 1) ? ", " : ""); //Add the name, and add space if there are still more names to add
+        }
+
+        return sb.toString();
+    }
+
+    // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
+    @Override
+    public void updateDescription() {
+        if (cardArray.size() == 0) {
+            description = DESCRIPTIONS[0];
+        } else {
+            description = DESCRIPTIONS[0] + DESCRIPTIONS[1] + lastName + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new EncorePower(owner, amount);
+    }
+}

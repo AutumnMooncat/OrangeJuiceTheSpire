c9538b218a3dbab1796469ce84ef30de5e95563f
MistressAlison <alisonmoons@gmail.com>
2021-01-19 21:06:16 -0400

    Plushie Mark rework, now raises 7 to damage, then if damage is 7 or less (can happen due to fixed damage) and there is block to pierce, it pierces. If no pierce was performed, then temp HP is gained.

--- OrangeJuiceTheSpire/src/main/java/Moonworks/powers/PlushieMasterPower.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/powers/PlushieMasterPower.java
@@ -1,6 +1,7 @@
 package Moonworks.powers;
 
 import Moonworks.OrangeJuiceMod;
+import Moonworks.patches.FixedPatches;
 import Moonworks.patches.PiercingPatches;
 import basemod.interfaces.CloneablePowerInterface;
 import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
@@ -9,7 +10,6 @@ import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import org.apache.logging.log4j.LogManager;
@@ -26,16 +26,14 @@ public class PlushieMasterPower extends AbstractTrapPower implements CloneablePo
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     private final int heal;
-    private final int damage;
-    private boolean increasedDamage;
-    private boolean indirectDamage;
-    private int previousBlock;
+    private final int damageThreshold;
+
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
     //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
-    public PlushieMasterPower(final AbstractCreature owner, final AbstractCreature source, final int amount, final int heal, final int damage) {
+    public PlushieMasterPower(final AbstractCreature owner, final AbstractCreature source, final int amount, final int heal, final int damageThreshold) {
         name = NAME;
         ID = POWER_ID;
 
@@ -43,14 +41,13 @@ public class PlushieMasterPower extends AbstractTrapPower implements CloneablePo
         this.amount = amount;
         this.source = source;
         this.heal = heal;
-        this.damage = damage;
+        this.damageThreshold = damageThreshold;
         this.type = PowerType.BUFF;
         this.priority = Integer.MAX_VALUE;
         this.isTurnBased = false;
         this.canGoNegative = false;
 
         // We load those txtures here.
-        //this.loadRegion("retain");
         this.loadRegion("regrow");
         //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
@@ -60,44 +57,41 @@ public class PlushieMasterPower extends AbstractTrapPower implements CloneablePo
 
     @Override
     public float atDamageFinalReceive(float damage, DamageInfo.DamageType type) {
-        if(!this.owner.isDead && this.amount > 0) {
-            if (damage < this.damage) {
-                damage = this.damage;
-                increasedDamage = true;
-                previousBlock = owner.currentBlock;
+        if (this.amount > 0) {
+            if (damage < this.damageThreshold) {
+                damage = this.damageThreshold;
             }
         }
-        indirectDamage = false;
         return super.atDamageFinalReceive(damage, type);
     }
 
     @Override
     public int onAttacked(DamageInfo info, int damageAmount) {
+        /*logger.info("Piercing? " + PiercingPatches.PiercingField.piercing.get(info));
+        logger.info("New Block: " + owner.currentBlock);
+        logger.info("Passed Damage Amount: " + damageAmount);
+        logger.info("Passed Damage Info Amount: " + info.output);
+        logger.info("Delta Damage: " + (info.output - damageAmount));*/
         if(!this.owner.isDead && this.amount > 0 && !PiercingPatches.PiercingField.piercing.get(info)) {
 
             //Needed for piercing and indirect calculations to know how much to modify by
-            int blockDelta = previousBlock - owner.currentBlock;
+            //int blockDelta = previousBlock - owner.currentBlock;
+            int blockDelta = info.output - damageAmount;
 
-            //If we did indirect damage (an attack that did not actually calculate damage on this target, we can do stuff
-            if (indirectDamage) {
-                //logger.info("Indirect Damage dealt.");
-            }
-
-            //If a damage boost as applied, we want to also pierce
-            if (increasedDamage) {
+            //If a damage boost was applied, we want to also pierce
+            if (info.output <= this.damageThreshold && blockDelta > 0) {
                 //The amount of damage that was actually blocked is our pierce amount
-                if (blockDelta > 0) {
-                    //Set up a damage info with the piecing flag.
-                    DamageInfo pierceDamage = new DamageInfo(source, blockDelta, DamageInfo.DamageType.HP_LOSS);
-                    PiercingPatches.PiercingField.piercing.set(pierceDamage, true);
-                    this.addToTop(new DamageAction(owner, pierceDamage, AbstractGameAction.AttackEffect.NONE, true));
-                }
+                //Set up a damage info with the piecing flag.
+                DamageInfo pierceDamage = new DamageInfo(source, blockDelta, DamageInfo.DamageType.HP_LOSS);
+                PiercingPatches.PiercingField.piercing.set(pierceDamage, true);
+                this.addToTop(new DamageAction(owner, pierceDamage, AbstractGameAction.AttackEffect.NONE, true));
             } else {
-                //If the damage was not increased, we want Temp HP
+                //If the damage was not increased or they had no Block, we want Temp HP
                 this.addToBot(new AddTemporaryHPAction(source, source, this.heal));
             }
 
             //Decrement and remove our power. Decrement action is not used as even with ToTop it caused issue with multi hit attacks
+            flash();
             this.amount--;
             if (this.amount <= 0) {
                 this.amount = 0;
@@ -105,13 +99,6 @@ public class PlushieMasterPower extends AbstractTrapPower implements CloneablePo
             }
             updateDescription();
         }
-        //Package this in an action so multi hit attacks don't get messed up
-        this.addToBot(new AbstractGameAction() {
-            public void update() {
-                increasedDamage = false;
-                indirectDamage = true;
-                this.isDone = true;
-            }});
         return damageAmount;
     }
 
@@ -119,14 +106,14 @@ public class PlushieMasterPower extends AbstractTrapPower implements CloneablePo
     @Override
     public void updateDescription() {
         if (amount == 1) {
-            description = DESCRIPTIONS[0] + heal + DESCRIPTIONS[1] + damage + DESCRIPTIONS[2];
+            description = DESCRIPTIONS[0] + heal + DESCRIPTIONS[1] + damageThreshold + DESCRIPTIONS[2];
         } else {
-            description = DESCRIPTIONS[0] + heal + DESCRIPTIONS[1] + damage + DESCRIPTIONS[3] + amount + DESCRIPTIONS[4];
+            description = DESCRIPTIONS[0] + heal + DESCRIPTIONS[1] + damageThreshold + DESCRIPTIONS[3] + amount + DESCRIPTIONS[4];
         }
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new PlushieMasterPower(owner, source, amount, heal, damage);
+        return new PlushieMasterPower(owner, source, amount, heal, damageThreshold);
     }
 }

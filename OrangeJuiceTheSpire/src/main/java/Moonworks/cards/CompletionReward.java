97ddc3c1e082e599ba2d6bf949d4127ae033fad5
MistressAlison <alisonmoons@gmail.com>
2021-01-26 12:35:13 -0400

    Completion Reward buff/nerf - Now costs 2(1) but does not reset

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/CompletionReward.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/CompletionReward.java
@@ -37,10 +37,12 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
 
-    private static final int COST = 1;
+    private static final int COST = 2;
+    private static final int UPGRADE_COST = 1;
     private int lastCount; //How many cards ad been played last time this card was played. Defaults to 0 since we haven't played the card yet.
 
     private static final Integer[] NORMA_LEVELS = {2};
+    private static final int NORMA_BOOST = 6;
 
     // /STAT DECLARATION/
 
@@ -48,7 +50,7 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, NORMA_LEVELS);
         //upgraded = true;
         //lastCount = AbstractDungeon.actionManager.cardsPlayedThisCombat.size(); //Buff: Card wont start at 0 damage if generated in combat
-        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - lastCount;//Damage is the number of cards played since the last time this card was played
+        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size();//Damage is the number of cards played since the last time this card was played
         CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INFOMOD, 1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
         initializeDescription();
     }
@@ -59,19 +61,14 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
         //logger.info("Use: last count: " + lastCount);
         //We have to -1 or else it counts the damage from this card being played as well
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage-1, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        if(upgraded) {
-            lastCount = AbstractDungeon.actionManager.cardsPlayedThisCombat.size()/2;
-        } else {
-            lastCount = AbstractDungeon.actionManager.cardsPlayedThisCombat.size();
-        }
-        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - lastCount + (getNormaLevel() >= NORMA_LEVELS[0] ? 3 : 0);
+        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() + (getNormaLevel() >= NORMA_LEVELS[0] ? NORMA_BOOST : 0);
         //logger.info("Used: last count: " + lastCount);
         initializeDescription();
     }
 
     public void applyPowers() {
         //logger.info("Apply: last count: " + lastCount);
-        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - lastCount + (getNormaLevel() >= NORMA_LEVELS[0] ? 3 : 0);
+        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() + (getNormaLevel() >= NORMA_LEVELS[0] ? NORMA_BOOST : 0);
         super.applyPowers();
         initializeDescription();
 
@@ -79,7 +76,7 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
 
     public void calculateCardDamage(AbstractMonster m) {
         //logger.info("Calc: last count: " + lastCount);
-        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - lastCount + (getNormaLevel() >= NORMA_LEVELS[0] ? 3 : 0);
+        this.baseDamage = AbstractDungeon.actionManager.cardsPlayedThisCombat.size() + (getNormaLevel() >= NORMA_LEVELS[0] ? NORMA_BOOST : 0);
         super.calculateCardDamage(m);
         initializeDescription();
 
@@ -95,7 +92,8 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            rawDescription = UPGRADE_DESCRIPTION;
+            //rawDescription = UPGRADE_DESCRIPTION;
+            upgradeBaseCost(UPGRADE_COST);
             //this.upgradeDamage(UPGRADE_PLUS_DAMAGE);
             this.initializeDescription();
         }

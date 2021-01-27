69b5d4072e2c8e26e5cf4b9798fa6ca27a32af74
MistressAlison <alisonmoons@gmail.com>
2021-01-27 15:45:50 -0400

    BlastingLoseHPAction now properly plays it's animation. Added animation stacking but commented it out for now

--- OrangeJuiceTheSpire/src/main/java/Moonworks/actions/BlastingLoseHPAction.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/actions/BlastingLoseHPAction.java
@@ -2,6 +2,7 @@ package Moonworks.actions;
 
 import Moonworks.powers.BlastingLightPower;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -29,14 +30,21 @@ public class BlastingLoseHPAction extends AbstractGameAction {
         if (AbstractDungeon.getCurrRoom().phase != AbstractRoom.RoomPhase.COMBAT) {
             this.isDone = true;
         } else {
-            if (this.duration == 0.33F && this.target.currentHealth > 0) {
+            if (this.duration == 0.1F && this.target.currentHealth > 0) {
                 AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, this.attackEffect));
+                /*int bonusHits = Math.max(1, MathUtils.floor(amount/5f)) - 1;
+                for (int i = 0 ; i < bonusHits ; i++) {
+                    int dx = MathUtils.random(-100, 100);
+                    int dy = MathUtils.random(-100, 100);
+                    AbstractDungeon.effectList.add(new FlashAtkImgEffect(target.hb.cX+dx, target.hb.cY+dy, attackEffect, true));
+                }*/
+
             }
 
             this.tickDuration();
             if (this.isDone) {
                 if (this.target.currentHealth > 0) {
-                    this.target.tint.color = Color.CHARTREUSE.cpy();
+                    this.target.tint.color = Color.ORANGE.cpy();
                     this.target.tint.changeColor(Color.WHITE.cpy());
                     this.target.damage(new DamageInfo(this.source, this.amount, DamageInfo.DamageType.HP_LOSS));
                     this.target.decreaseMaxHealth(this.amount);

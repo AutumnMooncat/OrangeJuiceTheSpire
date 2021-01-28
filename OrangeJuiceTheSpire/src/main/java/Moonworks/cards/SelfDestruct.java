0a5b12f4f3e9645fc4c431de4fe990ff3c72eac7
MistressAlison <alisonmoons@gmail.com>
2021-01-27 20:58:23 -0400

    SelfDestruct rework - Now deals fixed damage to yourself, and non-fixed damage to ALL enemies. Will be readdressed to add an option to toggle a more suicidal effect for thematic purposes.

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/SelfDestruct.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/SelfDestruct.java
@@ -2,20 +2,30 @@ package Moonworks.cards;
 
 import Moonworks.cardModifiers.NormaDynvarModifier;
 import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
+import Moonworks.cards.interfaces.RangedAttack;
+import Moonworks.patches.PowerBypassHelper;
+import Moonworks.patches.RangedPatches;
 import basemod.helpers.CardModifierManager;
-import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import basemod.helpers.TooltipInfo;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import Moonworks.OrangeJuiceMod;
 import Moonworks.characters.TheStarBreaker;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.EntanglePower;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static Moonworks.OrangeJuiceMod.makeCardPath;
 
-public class SelfDestruct extends AbstractNormaAttentiveCard {
+public class SelfDestruct extends AbstractNormaAttentiveCard implements RangedAttack {
 
     // TEXT DECLARATION
 
@@ -37,35 +47,90 @@ public class SelfDestruct extends AbstractNormaAttentiveCard {
     private static final int BLOCK = 10;
     private static final int UPGRADE_PLUS_BLOCK = 5;
 
-    private static final Integer[] NORMA_LEVELS = {3};
+    private static final int DAMAGE = 30;
+    private static final int UPGRADE_PLUS_DAMAGE = 10;
+    private static final int SELF_DAMAGE = 30;
+
+    private static final int BLOCK_REDUCTION = 2;
+    private static final int UPGRADE_PLUS_BLOCK_REDUCTION = 1;
+
+    private static final Integer[] NORMA_LEVELS = {-1};
+    protected final ArrayList<String> bypassList = new ArrayList<>();
 
     // /STAT DECLARATION/
 
     public SelfDestruct() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, NORMA_LEVELS);
-        block = baseBlock = BLOCK;
-        this.isMultiDamage = true;
-        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INFOMOD, 1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
+        //block = baseBlock = BLOCK;
+        damage = baseDamage = DAMAGE;
+        invertedNumber = baseInvertedNumber = SELF_DAMAGE;
+        secondMagicNumber = baseSecondMagicNumber = BLOCK_REDUCTION;
+        isMultiDamage = true;
+
+        bypassList.add(EntanglePower.POWER_ID);
+
+        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INVERTEDMOD, -1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new GainBlockAction(p, block));
-        this.addToBot(new DamageAction(p, new DamageInfo(p, AbstractDungeon.player.currentHealth/2, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.FIRE));
+        /*this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new DamageAction(p, new DamageInfo(p, AbstractDungeon.player.currentHealth/2, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.FIRE));
         this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
         if (getNormaLevel() >= 3) {
             this.addToBot(new AddTemporaryHPAction(p, p, 10));
+        }*/
+
+        //Bolster current Block before the self damage hits
+        this.addToBot(new AbstractGameAction() {
+            public void update() {
+                p.currentBlock *= secondMagicNumber;
+                this.isDone = true;
+            }});
+
+        //Hit oneself
+        this.addToBot(new DamageAction(p, new DamageInfo(p, invertedNumber, DamageInfo.DamageType.THORNS), true));
+
+        //Reset Block back to normal
+        this.addToBot(new AbstractGameAction() {
+            public void update() {
+                if (p.currentBlock > 0 && secondMagicNumber > 0) {
+                    //Let it be 1 at minimum so we don't muck around with a 0 block amount that is still visible.
+                    p.currentBlock = Math.max(1, MathUtils.floor((float)p.currentBlock / secondMagicNumber));
+                }
+                this.isDone = true;
+            }});
+
+        //Deal damage to everyone
+        //this.addToBot(new DamageAllEnemiesAction(p, multiDamage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.FIRE));
+
+        //A for loop rather than for-each would be more reasonable. Oh well.
+        int i = 0;
+        //Loop through every monster and hit them with the appropriate damage value
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                DamageInfo rangedDamage = new DamageInfo(p, multiDamage[i], damageTypeForTurn);
+                RangedPatches.RangedField.ranged.set(rangedDamage, true);
+                this.addToBot(new DamageAction(aM, rangedDamage, AbstractGameAction.AttackEffect.FIRE, true));
+            }
+            i++;
+        }
+
+        //Ensure powers that care about hitting all targets function properly
+        for (AbstractPower pow : p.powers) {
+            pow.onDamageAllEnemies(this.multiDamage);
         }
     }
-    public void applyPowers() {
-        this.baseDamage = AbstractDungeon.player.currentHealth/2;
-        super.applyPowers();
+
+    @Override //Allow this card to be played when Entangled
+    public boolean hasEnoughEnergy() {
+        return (boolean) PowerBypassHelper.performBypass(super::hasEnoughEnergy, bypassList, AbstractDungeon.player);
     }
 
-    public void calculateCardDamage(AbstractMonster m) {
-        this.baseDamage = AbstractDungeon.player.currentHealth/2;
-        super.calculateCardDamage(m);
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
     }
 
     // Upgraded stats.
@@ -73,8 +138,26 @@ public class SelfDestruct extends AbstractNormaAttentiveCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeBlock(UPGRADE_PLUS_BLOCK);
+            //this.upgradeBlock(UPGRADE_PLUS_BLOCK);
+            //this.upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            this.upgradeSecondMagicNumber(UPGRADE_PLUS_BLOCK_REDUCTION);
             this.initializeDescription();
         }
+    }
+
+    @Override
+    public List<String> getCardDescriptors() {
+        List<String> tags = new ArrayList<>();
+        tags.add(rangedTag);
+        tags.addAll(super.getCardDescriptors());
+        return tags;
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltipsTop() {
+        List<TooltipInfo> compoundList = new ArrayList<>();
+        compoundList.add(rangedTooltipInfo);
+        if (super.getCustomTooltipsTop() != null) compoundList.addAll(super.getCustomTooltipsTop());
+        return compoundList;
     }
 }

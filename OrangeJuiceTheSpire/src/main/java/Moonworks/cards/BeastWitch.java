ae80b39105b0a2dbbcf3f29200f4311ae0bcdcea
MistressAlison <alisonmoons@gmail.com>
2021-01-31 23:37:12 -0400

    Added new Common, BeastWitch

---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/BeastWitch.java
@@ -0,0 +1,90 @@
+package Moonworks.cards;
+
+import Moonworks.OrangeJuiceMod;
+import Moonworks.cardModifiers.NormaDynvarModifier;
+import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
+import Moonworks.characters.TheStarBreaker;
+import Moonworks.powers.TemporaryStrengthPower;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Moonworks.OrangeJuiceMod.makeCardPath;
+
+public class BeastWitch extends AbstractNormaAttentiveCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = OrangeJuiceMod.makeID(BeastWitch.class.getSimpleName());
+    public static final String IMG = makeCardPath("BeastWitch.png");
+
+    //private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    //public static final String SPENT_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
+
+    private static final int COST = 1;
+
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DAMAGE = 2;
+
+    private static final int TEMP_STR = 1;
+    private static final int UPGRADE_PLUS_TEMP_STR = 1;
+
+    private static final Integer[] NORMA_LEVELS = {2};
+
+
+    // /STAT DECLARATION/
+
+
+    public BeastWitch() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.damage = this.baseDamage = DAMAGE;
+        this.magicNumber = this.baseMagicNumber = TEMP_STR;
+        this.returnToHand = true;
+        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.DAMAGEMOD, 2, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
+
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //Deal damage
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+
+        //Gain Temp STR
+        this.addToBot(new ApplyPowerAction(p, p, new TemporaryStrengthPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            //upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            upgradeMagicNumber(UPGRADE_PLUS_TEMP_STR);
+            initializeDescription();
+        }
+    }
+}

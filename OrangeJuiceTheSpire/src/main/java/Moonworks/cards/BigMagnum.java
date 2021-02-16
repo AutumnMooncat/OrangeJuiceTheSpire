9ab6dd746e86450574facac70d5ddb2c53243057
MistressAlison <alisonmoons@gmail.com>
2021-02-16 19:41:12 -0400

    BigMagnum rework, now an Uncommon 1 Cost that Exhausts a card and gives 4 Vigor. The Vigor amount then increases by 4 each time it is played this combat.

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/BigMagnum.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/BigMagnum.java
@@ -1,18 +1,22 @@
 package Moonworks.cards;
 
 import Moonworks.OrangeJuiceMod;
+import Moonworks.actions.BigMagnumAction;
 import Moonworks.cardModifiers.NormaDynvarModifier;
 import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
 import Moonworks.characters.TheStarBreaker;
+import Moonworks.powers.TemporaryStrengthPower;
 import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static Moonworks.OrangeJuiceMod.makeCardPath;
 
@@ -27,81 +31,84 @@ public class BigMagnum extends AbstractNormaAttentiveCard {
     public static final String NAME = cardStrings.NAME;
     public static final String UPGRADE1NAME = cardStrings.EXTENDED_DESCRIPTION[1];
     public static final String UPGRADE2NAME = cardStrings.EXTENDED_DESCRIPTION[2];
+    public static final String UPGRADE3NAME = cardStrings.EXTENDED_DESCRIPTION[3];
+    public static final String UPGRADE4NAME = cardStrings.EXTENDED_DESCRIPTION[4];
+    public static final String UPGRADE5NAME = cardStrings.EXTENDED_DESCRIPTION[5];
+
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
 
-    private static final int COST = 0;
+    private static final int COST = 1;
 
-    private static final int HPLOSS = 5;
-    private static final int UPGRADE_PLUS_HPLOSS = 1;
+    private static final int VIGOR = 4;
 
-    private static final int VIGOR = 7;
-    private static final int UPGRADE_PLUS_VIGOR = 3;
+    private static final int VIGOR_SCALE = 4;
+    private static final int UPGRADE_PLUS_VIGOR_SCALE = 2;
 
-    private static final int STR = 0;
-    private static final int UPGRADE_PLUS_STR = 1;
-
-    private static final Integer[] NORMA_LEVELS = {-1};
+    private static final Integer[] NORMA_LEVELS = {3};
 
     // /STAT DECLARATION/
 
     public BigMagnum() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, NORMA_LEVELS);
-        invertedNumber = baseInvertedNumber = HPLOSS;
         magicNumber = baseMagicNumber = VIGOR;
-        secondMagicNumber = baseSecondMagicNumber = STR;
-        this.exhaust = true;
-        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.SECONDMAGICMOD, 1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
+        secondMagicNumber = baseSecondMagicNumber = VIGOR_SCALE;
+        CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INFOMOD, 1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new LoseHPAction(p, p, invertedNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, magicNumber), magicNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, secondMagicNumber), secondMagicNumber));
-    }
-
-    @Override
-    public void applyPowers() {
-        super.applyPowers();
-        this.secondMagicNumber = this.baseSecondMagicNumber;
-        this.isSecondMagicNumberModified = false;
-        initializeDescription();
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster m) {
-        super.calculateCardDamage(m);
-        this.secondMagicNumber = this.baseSecondMagicNumber + getNormaLevel();
-        this.isSecondMagicNumberModified = getNormaLevel() != 0;
-        initializeDescription();
+        boolean normaCheck = getNormaLevel() >= NORMA_LEVELS[0];
+        this.addToBot(new BigMagnumAction(this, normaCheck, EXTENDED_DESCRIPTION[6]));
     }
 
     @Override
     protected void upgradeName() {
         ++this.timesUpgraded;
         this.upgraded = true;
-        this.name = UPGRADE1NAME;
+        switch (timesUpgraded) {
+            case 1:
+                this.name = UPGRADE1NAME;
+                break;
+            case 2:
+                this.name = UPGRADE2NAME;
+                break;
+            case 3:
+                this.name = UPGRADE3NAME;
+                break;
+            case 4:
+                this.name = UPGRADE4NAME;
+                break;
+            case 5:
+            default:
+                this.name = UPGRADE5NAME;
+                break;
+
+        }
+
         this.initializeTitle();
+    }
+
+    @Override
+    public boolean canUpgrade() {
+        return this.timesUpgraded < 5;
     }
 
     // Upgraded stats.
     @Override
     public void upgrade() {
-        if (!this.upgraded) {
+        if (canUpgrade()) {
             this.upgradeName();
-            this.upgradeMagicNumber(UPGRADE_PLUS_VIGOR);
-            this.upgradeInvertedNumber(UPGRADE_PLUS_HPLOSS);
-            this.upgradeSecondMagicNumber(UPGRADE_PLUS_STR);
+            this.upgradeSecondMagicNumber(UPGRADE_PLUS_VIGOR_SCALE);
             this.initializeDescription();
         }
     }

7d948e1e84da7f8cb5eafeb9bde98ce9727cf0a8
MistressAlison <alisonmoons@gmail.com>
2021-02-19 18:45:20 -0400

    Now simply a healing Gift, no longer associated with Saki's Cookie

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/giftCards/SweetBattle.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/giftCards/SweetBattle.java
@@ -1,13 +1,11 @@
 package Moonworks.cards.giftCards;
 
 import Moonworks.OrangeJuiceMod;
-import Moonworks.cards.SakisCookie;
 import Moonworks.cards.abstractCards.AbstractGiftCard;
 import Moonworks.characters.TheStarBreaker;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -38,26 +36,28 @@ public class SweetBattle extends AbstractGiftCard {
     private static final int USES = 3;
     private static final int UPGRADE_PLUS_USES = 1;
 
+    private static final int HEAL = 2;
+    private static final int UPGRADE_PLUS_HEAL = 1;
+
     // /STAT DECLARATION/
 
-
     public SweetBattle() {
-
         this(USES, false);
     }
 
     public SweetBattle(int currentUses, boolean checkedGolden) {
-
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, USES, currentUses, checkedGolden);
-        this.magicNumber = this.baseMagicNumber = CARDS;
-        this.cardsToPreview = new SakisCookie();
+        this.magicNumber = this.baseMagicNumber = HEAL;
+        //this.cardsToPreview = new SakisCookie();
+        this.tags.add(CardTags.HEALING);
     }
 
     @Override
     public void triggerWhenDrawn() {
         super.triggerWhenDrawn();
         if(isActive(true)) {
-            this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy()));
+            this.addToBot(new HealAction(AbstractDungeon.player, AbstractDungeon.player, magicNumber));
+            //this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy()));
             //this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview.makeStatEquivalentCopy(), 1, true, true));
             this.applyEffect();
         }
@@ -67,7 +67,8 @@ public class SweetBattle extends AbstractGiftCard {
     public void atTurnStartPreDraw() {
         super.atTurnStartPreDraw();
         if (isActive()) {
-            this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy()));
+            this.addToBot(new HealAction(AbstractDungeon.player, AbstractDungeon.player, magicNumber));
+            //this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy()));
             //this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview.makeStatEquivalentCopy(), 1, true, true));
             this.applyEffect();
         }
@@ -78,9 +79,9 @@ public class SweetBattle extends AbstractGiftCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            this.cardsToPreview.upgrade();
-            rawDescription = UPGRADE_DESCRIPTION;
-            //upgradeSecondMagicNumber(UPGRADE_PLUS_USES);
+            //this.cardsToPreview.upgrade();
+            //rawDescription = UPGRADE_DESCRIPTION;
+            upgradeSecondMagicNumber(UPGRADE_PLUS_USES);
             initializeDescription();
         }
     }

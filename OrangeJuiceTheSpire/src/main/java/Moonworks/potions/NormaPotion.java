87fd7e1982a1444540a113539f392fb0c8c9871b
MistressAlison <alisonmoons@gmail.com>
2021-02-20 14:07:48 -0400

    Added Norma generating potion

---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/potions/NormaPotion.java
@@ -0,0 +1,59 @@
+package Moonworks.potions;
+
+import Moonworks.OrangeJuiceMod;
+import Moonworks.powers.NormaGainPower;
+import Moonworks.powers.NormaPower;
+import basemod.abstracts.CustomPotion;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+
+public class NormaPotion extends CustomPotion {
+
+
+    public static final String POTION_ID = OrangeJuiceMod.makeID("NormaPotion");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public NormaPotion() {
+        // The bottle shape and inside is determined by potion size and color. The actual colors are the main DefaultMod.java
+        super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.BOTTLE, PotionColor.FIRE);
+
+        // Do you throw this potion at an enemy or do you just consume it.
+        isThrown = false;
+    }
+
+    @Override
+    public void use(AbstractCreature target) {
+        this.getPotency(AbstractDungeon.ascensionLevel);
+        AbstractPlayer p = AbstractDungeon.player;
+        this.addToBot(new ApplyPowerAction(p, p, new NormaGainPower(p, potency)));
+    }
+
+    // This is your potency.
+    @Override
+    public int getPotency(final int ascensionLevel) {
+        return 2;
+    }
+
+    @Override
+    public void initializeData() {
+        potency = getPotency();
+        description = potionStrings.DESCRIPTIONS[0] + potency + potionStrings.DESCRIPTIONS[1];
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        tips.add(new PowerTip(NormaPower.NAME,NormaPower.DESCRIPTIONS[0]));
+    }
+
+    @Override
+    public AbstractPotion makeCopy() {
+        return new NormaPotion();
+    }
+}

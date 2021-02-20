3d5aceb9917b82dee628e67bd9094bda83c86222
MistressAlison <alisonmoons@gmail.com>
2021-02-20 14:07:37 -0400

    Added Vigor and Steady potions

---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/potions/SteadyPotion.java
@@ -0,0 +1,61 @@
+package Moonworks.potions;
+
+import Moonworks.OrangeJuiceMod;
+import Moonworks.powers.SteadyPower;
+import basemod.BaseMod;
+import basemod.abstracts.CustomPotion;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+
+public class SteadyPotion extends CustomPotion {
+
+
+    public static final String POTION_ID = OrangeJuiceMod.makeID("SteadyPotion");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public SteadyPotion() {
+        // The bottle shape and inside is determined by potion size and color. The actual colors are the main DefaultMod.java
+        super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.FAIRY, PotionColor.GREEN);
+
+        // Do you throw this potion at an enemy or do you just consume it.
+        isThrown = false;
+    }
+
+    @Override
+    public void use(AbstractCreature target) {
+        this.getPotency(AbstractDungeon.ascensionLevel);
+        AbstractPlayer p = AbstractDungeon.player;
+        this.addToBot(new ApplyPowerAction(p, p, new SteadyPower(p, potency)));
+    }
+
+    @Override
+    public int getPotency(final int ascensionLevel) {
+        return 10;
+    }
+
+    @Override
+    public void initializeData() {
+        potency = getPotency();
+        description = potionStrings.DESCRIPTIONS[0] + potency + potionStrings.DESCRIPTIONS[1];
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        tips.add(new PowerTip(
+                BaseMod.getKeywordTitle("moonworks:steady"),
+                BaseMod.getKeywordDescription("moonworks:steady")
+        ));
+    }
+
+    @Override
+    public AbstractPotion makeCopy() {
+        return new SteadyPotion();
+    }
+}
---
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/potions/VigorPotion.java
@@ -0,0 +1,62 @@
+package Moonworks.potions;
+
+import Moonworks.OrangeJuiceMod;
+import basemod.BaseMod;
+import basemod.abstracts.CustomPotion;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+public class VigorPotion extends CustomPotion {
+
+
+    public static final String POTION_ID = OrangeJuiceMod.makeID("VigorPotion");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public VigorPotion() {
+        // The bottle shape and inside is determined by potion size and color. The actual colors are the main DefaultMod.java
+        super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.FAIRY, PotionColor.FIRE);
+
+        // Do you throw this potion at an enemy or do you just consume it.
+        isThrown = false;
+    }
+
+    @Override
+    public void use(AbstractCreature target) {
+        this.getPotency(AbstractDungeon.ascensionLevel);
+        AbstractPlayer p = AbstractDungeon.player;
+        this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, potency)));
+    }
+
+    // This is your potency.
+    @Override
+    public int getPotency(final int ascensionLevel) {
+        return 10;
+    }
+
+    @Override
+    public void initializeData() {
+        potency = getPotency();
+        description = potionStrings.DESCRIPTIONS[0] + potency + potionStrings.DESCRIPTIONS[1];
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        tips.add(new PowerTip(
+                BaseMod.getKeywordTitle("vigor"),
+                BaseMod.getKeywordDescription("vigor")
+        ));
+    }
+
+    @Override
+    public AbstractPotion makeCopy() {
+        return new VigorPotion();
+    }
+}

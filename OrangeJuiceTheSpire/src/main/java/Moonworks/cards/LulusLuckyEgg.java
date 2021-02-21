30aef211461d5912cc7e8510ec5ade56d4f656af
MistressAlison <alisonmoons@gmail.com>
2021-02-21 17:26:13 -0400

    Added support for "Luckiest" Egg which performs all actions

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/LulusLuckyEgg.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/LulusLuckyEgg.java
@@ -5,8 +5,11 @@ import Moonworks.powers.TemporaryDexterityPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import Moonworks.OrangeJuiceMod;
 import Moonworks.characters.TheStarBreaker;
@@ -29,7 +32,7 @@ public class LulusLuckyEgg extends AbstractDynamicCard {
     public static final String ID = OrangeJuiceMod.makeID(LulusLuckyEgg.class.getSimpleName());
     public static final String IMG = makeCardPath("LulusLuckyEgg.png");
 
-    //private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     //public static final String SPENT_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
@@ -49,36 +52,66 @@ public class LulusLuckyEgg extends AbstractDynamicCard {
     private static final int BONUS_EFFECT = 2;
     private static final int UPGRADE_PLUS_BONUS_EFFECT = 1;
 
+    private final boolean luckiest;
+
     // /STAT DECLARATION/
 
 
     public LulusLuckyEgg() {
+        this(false);
+
+    }
+
+    public LulusLuckyEgg(boolean luckiest) {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        //this.block = this.baseBlock = BLOCK;
         this.block = this.baseBlock = BASE_BLOCK;
         this.magicNumber = this.baseMagicNumber = BONUS_EFFECT;
-
+        this.luckiest = luckiest;
+        if (luckiest) {
+            this.name = cardStrings.EXTENDED_DESCRIPTION[0];
+        }
+        initializeDescription();
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int effect = AbstractDungeon.cardRandomRng.random(1, 3); //Choose a random number for the effect
-        switch (effect) { //Switches allow us to run code based on the value in the switch.
-            case 1: //Big Block. We multiply add block 1 or 2 more times here
-                //Loop for 1 less than the magic number, since we still block normally after
-                for (int i = 0 ; i < magicNumber - 1 ; i++) {
-                    this.addToBot(new GainBlockAction(p, p, block));
-                }
-                break; //Break after each case since we dont want it to then look at the other cases
-            case 2: //Draw 2 or 3 cards, as this is stored in magicNumber
-                this.addToBot(new DrawCardAction(magicNumber));
-                break;
-            case 3: //Buff 2 or 3 stacks of temp dex
-                this.addToBot(new ApplyPowerAction(p, p, new TemporaryDexterityPower(p, magicNumber)));
-                break;
+        if (luckiest) {
+            //Gain multi-blocks
+            for (int i = 0 ; i < magicNumber ; i++) {
+                this.addToBot(new GainBlockAction(p, p, block));
+            }
+            //Draw
+            this.addToBot(new DrawCardAction(magicNumber));
+            //Temp Dex
+            this.addToBot(new ApplyPowerAction(p, p, new TemporaryDexterityPower(p, magicNumber)));
+        } else {
+            int effect = AbstractDungeon.cardRandomRng.random(1, 3); //Choose a random number for the effect
+            switch (effect) { //Switches allow us to run code based on the value in the switch.
+                case 1: //Big Block. We multiply add block 1 or 2 more times here
+                    //Loop for 1 less than the magic number, since we still block normally after
+                    for (int i = 0 ; i < magicNumber - 1 ; i++) {
+                        this.addToBot(new GainBlockAction(p, p, block));
+                    }
+                    break; //Break after each case since we dont want it to then look at the other cases
+                case 2: //Draw 2 or 3 cards, as this is stored in magicNumber
+                    this.addToBot(new DrawCardAction(magicNumber));
+                    break;
+                case 3: //Buff 2 or 3 stacks of temp dex
+                    this.addToBot(new ApplyPowerAction(p, p, new TemporaryDexterityPower(p, magicNumber)));
+                    break;
+            }
+            this.addToBot(new GainBlockAction(p, p, block)); //Do our block action.
         }
-        this.addToBot(new GainBlockAction(p, p, block)); //Do our block action.
+
+    }
+
+    @Override
+    public void initializeDescription() {
+        if (luckiest) {
+            this.rawDescription = cardStrings.EXTENDED_DESCRIPTION[1];
+        }
+        super.initializeDescription();
     }
 
     //Upgraded stats.
@@ -90,5 +123,10 @@ public class LulusLuckyEgg extends AbstractDynamicCard {
             upgradeMagicNumber(UPGRADE_PLUS_BONUS_EFFECT);
             initializeDescription();
         }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new LulusLuckyEgg(luckiest);
     }
 }

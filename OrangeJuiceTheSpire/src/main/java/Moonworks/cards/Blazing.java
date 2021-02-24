77d86e4b568aa0ac6eadff7c7c322ac22cee6c44
MistressAlison <alisonmoons@gmail.com>
2021-02-24 19:51:46 -0400

    Blazing! rework, now applies Blasting Light each turn and scales

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/Blazing.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/Blazing.java
@@ -1,8 +1,11 @@
 package Moonworks.cards;
 
+import Moonworks.cardModifiers.NormaDynvarModifier;
 import Moonworks.cards.abstractCards.AbstractDynamicCard;
+import Moonworks.cards.abstractCards.AbstractNormaAttentiveCard;
 import Moonworks.powers.BlazingPower;
 import basemod.helpers.BaseModCardTags;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -10,10 +13,11 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import Moonworks.OrangeJuiceMod;
 import Moonworks.characters.TheStarBreaker;
+import com.megacrit.cardcrawl.powers.DexterityPower;
 
 import static Moonworks.OrangeJuiceMod.makeCardPath;
 
-public class Blazing extends AbstractDynamicCard {
+public class Blazing extends AbstractNormaAttentiveCard {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
@@ -40,27 +44,34 @@ public class Blazing extends AbstractDynamicCard {
 
     private static final int COST = 3;
 
-    private static final int STRENGTH = 3;
-    private static final int UPGRADE_PLUS_STRENGTH = 2;
+    private static final int BASE_BLAST = 6;
+    private static final int UPGRADE_PLUS_BASE_BLAST = 3;
 
-    private static final int DEXTERITY = -1;
-    private static final int UPGRADE_PLUS_DEXTERITY = -1;
+    private static final int BLAST_SCALE = 3;
+    private static final int UPGRADE_PLUS_BLAST_SCALE = 3;
+
+    //private static final int BAD_DEX = 1;
+
+    //private static final Integer[] NORMA_LEVELS = {3};
 
     // /STAT DECLARATION/
 
-
+    //TODO balance
     public Blazing() {
-
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        this.magicNumber = this.baseMagicNumber = STRENGTH;
-        this.secondMagicNumber = this.baseSecondMagicNumber = DEXTERITY;
+        this.magicNumber = this.baseMagicNumber = BASE_BLAST;
+        this.secondMagicNumber = this.baseSecondMagicNumber = BLAST_SCALE;
+        //this.invertedNumber = this.baseInvertedNumber = BAD_DEX;
         this.tags.add(BaseModCardTags.FORM); //Tag your strike, defend and form cards so that they work correctly.
-
+        //CardModifierManager.addModifier(this, new NormaDynvarModifier(NormaDynvarModifier.DYNVARMODS.INVERTEDMOD, -1, NORMA_LEVELS[0], EXTENDED_DESCRIPTION[0]));
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (p.hasPower(BlazingPower.POWER_ID)) {
+            ((BlazingPower)p.getPower(BlazingPower.POWER_ID)).updateScaling(secondMagicNumber);
+        }
         this.addToBot(new ApplyPowerAction(p ,p , new BlazingPower(p, magicNumber, secondMagicNumber)));
     }
 
@@ -70,8 +81,8 @@ public class Blazing extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             //rawDescription = UPGRADE_DESCRIPTION;
-            upgradeMagicNumber(UPGRADE_PLUS_STRENGTH);
-            upgradeSecondMagicNumber(UPGRADE_PLUS_DEXTERITY);
+            //upgradeMagicNumber(UPGRADE_PLUS_BASE_BLAST);
+            upgradeSecondMagicNumber(UPGRADE_PLUS_BLAST_SCALE);
             initializeDescription();
         }
     }
--- OrangeJuiceTheSpire/src/main/java/Moonworks/powers/BlazingPower.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/powers/BlazingPower.java
@@ -1,14 +1,24 @@
 package Moonworks.powers;
 
 import Moonworks.OrangeJuiceMod;
+import Moonworks.characters.TheStarBreaker;
 import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.combat.ScreenOnFireEffect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,20 +32,20 @@ public class BlazingPower extends AbstractPower implements CloneablePowerInterfa
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    private final int secondAmount;
+    private int secondAmount;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
     //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
-    public BlazingPower(final AbstractCreature owner, final int strength, final int dexterity) {
+    public BlazingPower(final AbstractCreature owner, final int blast, final int blastScale) {
         name = NAME;
         ID = POWER_ID;
 
         this.owner = owner;
-        this.amount = strength;
-        this.secondAmount = dexterity;
+        this.amount = blast;
+        this.secondAmount = blastScale;
 
         type = PowerType.BUFF;
         isTurnBased = false;
@@ -50,14 +60,58 @@ public class BlazingPower extends AbstractPower implements CloneablePowerInterfa
 
     public void atStartOfTurnPostDraw() {
         this.flash();
-        this.addToBot(new ApplyPowerAction(this.owner, this.owner, new StrengthPower(this.owner, this.amount), this.amount));
-        this.addToBot(new ApplyPowerAction(this.owner, this.owner, new DexterityPower(this.owner, this.secondAmount), this.secondAmount));
+        /*this.addToBot(new VFXAction(owner, new ScreenOnFireEffect(), 0.0F));
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.LOW, ScreenShake.ShakeDur.XLONG, false);
+                this.isDone = true;
+            }
+        });*/
+        //Play the happy animation
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                if (owner instanceof TheStarBreaker) {
+                    ((TheStarBreaker) owner).playAnimation("happy");
+                }
+                this.isDone = true;
+            }
+        });
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            AbstractPower pow = aM.getPower(ArtifactPower.POWER_ID);
+            int artifactAmount = 0;
+            if (pow != null) {
+                artifactAmount = pow.amount;
+                this.addToTop(new ReducePowerAction(aM, owner, pow, amount));
+            }
+            if (amount > artifactAmount) {
+                this.addToBot(new ApplyPowerAction(aM, owner, new BlastingLightPower(aM, amount - artifactAmount), amount - artifactAmount, true));
+            }
+        }
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                scale();
+                this.isDone = true;
+            }
+        });
+    }
+
+    public void updateScaling (int secondAmount) {
+        this.secondAmount += secondAmount;
+        updateDescription();
+    }
+
+    private void scale() {
+        this.amount += this.secondAmount;
+        updateDescription();
     }
 
     // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + -secondAmount + DESCRIPTIONS[2];
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + secondAmount + DESCRIPTIONS[2];
     }
 
     @Override

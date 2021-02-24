a2fc12b68dbae7f8ddf680403d1036397b957267
MistressAlison <alisonmoons@gmail.com>
2021-02-24 17:48:16 -0400

    Added 100% Orange Juice (Potion), replacing old potion that was converted to 200% Orange Juice

--- OrangeJuiceTheSpire/src/main/java/Moonworks/potions/OneHundredPercentOrangeJuicePotion.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/potions/OneHundredPercentOrangeJuicePotion.java
@@ -1,13 +1,10 @@
 package Moonworks.potions;
 
 import Moonworks.OrangeJuiceMod;
-import Moonworks.actions.NormaBreakAction;
 import Moonworks.powers.NormaPower;
-import basemod.BaseMod;
+import Moonworks.util.NormaHelper;
 import basemod.abstracts.CustomPotion;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -15,25 +12,19 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.PotionStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import com.megacrit.cardcrawl.vfx.combat.ScreenOnFireEffect;
-
-import java.util.ArrayList;
 
 public class OneHundredPercentOrangeJuicePotion extends CustomPotion {
 
 
     public static final String POTION_ID = OrangeJuiceMod.makeID("OneHundredPercentOrangeJuicePotion");
     private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
-    
+
     public static final String NAME = potionStrings.NAME;
     public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
 
-    private boolean upgraded = false;
-
     public OneHundredPercentOrangeJuicePotion() {
         // The bottle shape and inside is determined by potion size and color. The actual colors are the main DefaultMod.java
-        super(NAME, POTION_ID, PotionRarity.RARE, PotionSize.BOTTLE, PotionColor.ENERGY);
+        super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.BOTTLE, PotionColor.FIRE);
 
         // Do you throw this potion at an enemy or do you just consume it.
         isThrown = false;
@@ -43,23 +34,20 @@ public class OneHundredPercentOrangeJuicePotion extends CustomPotion {
     public void use(AbstractCreature target) {
         this.getPotency(AbstractDungeon.ascensionLevel);
         AbstractPlayer p = AbstractDungeon.player;
-        this.addToBot(new NormaBreakAction(p, upgraded));
+        NormaHelper.applyNormaPowerNoTriggers(p, 5);
+        this.addToBot(new HealAction(p, p, potency));
     }
 
     // This is your potency.
     @Override
     public int getPotency(final int ascensionLevel) {
-    AbstractPlayer p = AbstractDungeon.player;
-        if (p != null && p.hasRelic("SacredBark")) {
-            upgraded = true;
-        }
-        return 6;
+        return 5;
     }
 
     @Override
     public void initializeData() {
         potency = getPotency();
-        description = potionStrings.DESCRIPTIONS[0] + potency + (upgraded ? potionStrings.DESCRIPTIONS[2] : potionStrings.DESCRIPTIONS[1]);
+        description = potionStrings.DESCRIPTIONS[0] + potency + potionStrings.DESCRIPTIONS[1];
         tips.clear();
         tips.add(new PowerTip(name, description));
         tips.add(new PowerTip(NormaPower.NAME,NormaPower.DESCRIPTIONS[0]));

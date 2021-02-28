a8c8e270cf762af52cc6351903385fbf200dfcb6
MistressAlison <alisonmoons@gmail.com>
2021-02-28 14:50:38 -0400

    Bloodlust rework

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/giftCards/Bloodlust.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/giftCards/Bloodlust.java
@@ -2,12 +2,14 @@ package Moonworks.cards.giftCards;
 
 import Moonworks.OrangeJuiceMod;
 import Moonworks.cards.abstractCards.AbstractGiftCard;
+import Moonworks.cards.interfaces.OnDealDamageCard;
+import Moonworks.cards.interfaces.OnLoseHPLastCard;
 import Moonworks.characters.TheStarBreaker;
+import Moonworks.powers.TemporaryStrengthPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import org.apache.logging.log4j.LogManager;
@@ -15,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 
 import static Moonworks.OrangeJuiceMod.makeCardPath;
 
-public class Bloodlust extends AbstractGiftCard {
+public class Bloodlust extends AbstractGiftCard implements OnDealDamageCard, OnLoseHPLastCard {
 
     public static final Logger logger = LogManager.getLogger(OrangeJuiceMod.class.getName());
 
@@ -35,65 +37,47 @@ public class Bloodlust extends AbstractGiftCard {
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
 
     private static final int COST = -2;
-    private static final int DAMAGE = 4;
-    //private static final int UPGRADE_PLUS_DAMAGE = -1; //Maybe do this instead of healing more?
-    private static final int HEAL = 2;
-    //private static final int UPGRADE_PLUS_HEAL = 1;
-    private static final int USES = 7;
-    private static final int UPGRADE_PLUS_USES = 3; // Maybe this?
+
+    private static final int TEMP_STR = 2;
+    private static final int UPGRADE_PLUS_TEMP_STR = 1;
+
+    private static final int USES = 4;
+    private static final int UPGRADE_PLUS_USES = 1; // Maybe this?
+
+    private boolean activatedThisTurn;
 
     //private static final int UPGRADE_PLUS_RETAINS = 1;
 
     // /STAT DECLARATION/
 
-
+    //TODO balance?
     public Bloodlust() {
-
         this(USES, false);
-
     }
 
     public Bloodlust(int currentUses, boolean checkedGolden) {
-
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, USES, currentUses, checkedGolden);
-        this.magicNumber = this.baseMagicNumber = HEAL;
-        this.tags.add(CardTags.HEALING);
-
+        this.magicNumber = this.baseMagicNumber = TEMP_STR;
     }
 
-    @Override
-    public void onPlayCard(AbstractCard c, AbstractMonster m) {
-        if(isActive()) {
-            AbstractPlayer p = AbstractDungeon.player;
-            if(c.type == CardType.ATTACK) {
-                this.addToBot(new HealAction(p, p, magicNumber));
-                this.applyEffect();
+    public void applyStrength() {
+        if (isActive()) {
+            this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new TemporaryStrengthPower(AbstractDungeon.player, magicNumber)));
+            if (!activatedThisTurn) {
+                activatedThisTurn = true;
             }
-        }
-        super.onPlayCard(c, m);
-    }
-
-    @Override
-    public void triggerWhenDrawn() {
-        super.triggerWhenDrawn();
-        if(isActive(true)) {
-            this.applyDamageEffect();
+            //If it already activated this turn, we still get the damage negation but we don't consume a use
+            flash();
         }
     }
 
     @Override
     public void atTurnStartPreDraw() {
         super.atTurnStartPreDraw();
-        if(isActive()) {
-            this.applyDamageEffect();
+        if (activatedThisTurn) {
+            activatedThisTurn = false;
+            applyEffect();
         }
-    }
-
-    public void applyDamageEffect() {
-        AbstractPlayer p = AbstractDungeon.player;
-        //int lessDamage = getNormaLevel() >= 2 ? 1 : 0;
-        this.addToBot(new DamageAction(p, new DamageInfo(p, DAMAGE, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.FIRE));
-        //Dont use a use here. This will result in us being able to hold this Gift as long as we want, but get a limited number of heals
     }
 
     //Upgraded stats.
@@ -101,8 +85,8 @@ public class Bloodlust extends AbstractGiftCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            //upgradeMagicNumber(UPGRADE_PLUS_HEAL);
-            upgradeSecondMagicNumber(UPGRADE_PLUS_USES);
+            upgradeMagicNumber(UPGRADE_PLUS_TEMP_STR);
+            //upgradeSecondMagicNumber(UPGRADE_PLUS_USES);
             initializeDescription();
         }
     }
@@ -110,5 +94,19 @@ public class Bloodlust extends AbstractGiftCard {
     @Override
     public AbstractCard makeCopy() {
         return new Bloodlust(secondMagicNumber, checkedGolden);
+    }
+
+    @Override
+    public int onDealDamage(AbstractMonster m, DamageInfo info, int damageAmount) {
+        applyStrength();
+        return damageAmount;
+    }
+
+    @Override
+    public int onLoseHpLast(DamageInfo info, int damageAmount) {
+        /*if (info.owner == AbstractDungeon.player) {
+            applyStrength();
+        }*/
+        return damageAmount;
     }
 }

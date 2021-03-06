f1e8e43f4c520627a99015c3b959948f94789a54
MistressAlison <alisonmoons@gmail.com>
2021-03-06 16:27:23 -0400

    Final Battle now Uncommon

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/FinalBattle.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/FinalBattle.java
@@ -33,7 +33,7 @@ public class FinalBattle extends AbstractDynamicCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ALL;
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;

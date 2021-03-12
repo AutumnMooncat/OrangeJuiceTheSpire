1e377c3d31de3ca17de49b48e3a51787b6ec030d
MistressAlison <alisonmoons@gmail.com>
2021-03-12 17:41:24 -0400

    CompletionReward moved to Common

--- OrangeJuiceTheSpire/src/main/java/Moonworks/cards/CompletionReward.java
+++ OrangeJuiceTheSpire/src/main/java/Moonworks/cards/CompletionReward.java
@@ -32,7 +32,7 @@ public class CompletionReward extends AbstractNormaAttentiveCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheStarBreaker.Enums.COLOR_WHITE_ICE;
